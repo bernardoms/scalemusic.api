621e03955c319ddae10057fd8f73908585dbed05
Marco Carvalho <marcolucio27@gmail.com>
2018-04-29 17:37:26 -0300

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require("./routes");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 
 routes(app);
 
 const port = process.env.PORT || 8080;
-app.listen(port);
-console.log(`todo list RESTful API server started on: ${port}`);
+app.listen(port, () => console.log(`todo list RESTful API server started on: ${port}`));
