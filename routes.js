29193ea18fc44c67643b1c00c3d445ed82e95586
Marco Carvalho <marcolucio27@gmail.com>
2018-04-29 23:20:37 -0300

    replace tonal-scale for tonal

--- routes.js
+++ routes.js
@@ -1,34 +1,34 @@
-const tonalScale = require("tonal-scale");
+const tonal = require("tonal");
 const noteParser = require('note-parser');
 
 module.exports = function (app) {
   // Returns the notes of a scale from a given note and tonic
-  app.get('/notes', (req, res) => {
-    res.send(tonalScale.notes(req.query.note, req.query.tonic));
+  app.get('/scale/notes', (req, res) => {
+    res.send(tonal.Scale.notes(req.query.note, req.query.tonic));
   });
 
   // Returns the chords of a given scale
-  app.get('/chords', (req, res) => {
-    res.send(tonalScale.chords(req.query.scale));
+  app.get('/scale/chords', (req, res) => {
+    res.send(tonal.Scale.chords(req.query.scale));
   });
 
   // Returns the intervals of a given scale
-  app.get('/intervals', (req, res) => {
-    res.send(tonalScale.intervals(req.query.scale));
+  app.get('/scale/intervals', (req, res) => {
+    res.send(tonal.Scale.intervals(req.query.scale));
   });
 
   // Returns scale names
-  app.get('/names', (req, res) => {
-    res.send(tonalScale.names());
+  app.get('/scale/names', (req, res) => {
+    res.send(tonal.Scale.names());
   });
 
   // Returns a json with properties of a scale
-  app.get('/props', (req, res) => {
-    res.send(tonalScale.props(req.query.scale));
+  app.get('/scale/props', (req, res) => {
+    res.send(tonal.Scale.props(req.query.scale));
   });
 
   // Returns info about a given note
-  app.get('/frequency', (req, res) => {
+  app.get('/scale/frequency', (req, res) => {
     let acc = "";
     if (req.query.acc === "sharp") {
       acc = "#";
@@ -36,5 +36,10 @@ module.exports = function (app) {
       acc = "b";
     }
     res.send(noteParser.parse(req.query.note + acc + req.query.oct));
+  });
+
+  //
+  app.get('/note/freq', (req, res) => {
+    res.send(tonal.Note.freq(req.query.note + req.query.oct));
   });
 };
