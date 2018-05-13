4933bc453741362e353b8ec732fb19aeff8a7165
Marco Carvalho <marcolucio27@gmail.com>
2018-05-12 23:27:26 -0300

    update routes

--- routes.js
+++ routes.js
@@ -2,38 +2,28 @@ const tonal = require("tonal");
 const noteParser = require('note-parser');
 
 module.exports = function (app) {
-  // Returns the notes of a scale from a given note and tonic
-  app.get('/scale/notes', (req, res) => {
-    res.send(tonal.Scale.notes(req.query.note, req.query.tonic));
-  });
-
-  // Returns the chords of a given scale
-  app.get('/scale/chords', (req, res) => {
-    res.send(tonal.Scale.chords(req.query.scale));
-  });
-
-  // Returns the intervals of a given scale
-  app.get('/scale/intervals', (req, res) => {
-    res.send(tonal.Scale.intervals(req.query.scale));
-  });
+  app.get('/scale/notes', (req, res) => res.send(tonal.Scale.notes(req.query.note, req.query.tonic)));
+  app.get('/scale/chords', (req, res) => res.send(tonal.Scale.chords(req.query.scale)));
+  app.get('/scale/intervals', (req, res) => res.send(tonal.Scale.intervals(req.query.scale)));
+  app.get('/scale/props', (req, res) => res.send(tonal.Scale.props(req.query.scale)));
+  app.get('/note/freq', (req, res) => res.send(tonal.Note.freq(req.query.note + req.query.oct)));
+  app.get('/chord/notes', (req, res) => res.send(tonal.Chord.notes(req.query.chord)));
 
   // Returns scale names
   app.get('/scale/names', (req, res) => {
-	let scaleArray = [];
-	if(req.query.isSimple){
-		scaleArray.push("Ionian (Maior)");
-		scaleArray.push("Dorian (Menor Natural)");
-		scaleArray.push("Phrygian (Menor Harmônica)");
-	}
-	else{
-		scaleArray = tonal.Scale.names()
-	}
-    res.send(scaleArray);
-  });
-
-  // Returns a json with properties of a scale
-  app.get('/scale/props', (req, res) => {
-    res.send(tonal.Scale.props(req.query.scale));
+    if(req.query.isSimple){
+      res.send([
+        "major",
+        "aeolian",
+        "dorian",
+        "phrygian",
+        "lydian",
+        "mixolydian",
+        "locrian",
+      ]);
+    } else {
+      res.send(tonal.Scale.names())
+    }
   });
 
   // Returns info about a given note
@@ -45,15 +35,5 @@ module.exports = function (app) {
       acc = "b";
     }
     res.send(noteParser.parse(req.query.note + acc + req.query.oct));
-  });
-
-  //
-  app.get('/note/freq', (req, res) => {
-    res.send(tonal.Note.freq(req.query.note + req.query.oct));
-  });
-
-  //
-  app.get('/chord/notes', (req, res) => {
-    res.send(tonal.Chord.notes(req.query.chord));
   });
 };
